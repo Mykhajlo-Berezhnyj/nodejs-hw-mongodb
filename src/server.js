799e22621ead4544c8616158313328c0f7a7edd7
Mykhajlo <bmve@ukr.net>
2025-07-04 21:28:01 +0300

    add version response

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import pino from 'pino-http';
 import cors from 'cors';
 import { config } from './config.js';
 import { getAllContacts, getContactsById } from './services/contacts.js';
+import { renderContactsList } from './utils/renderContactsList.js';
+import { renderContact } from './utils/renderContact.js';
+import { welcome } from './utils/wellcome.js';
 
 export const setupServer = () => {
   const app = express();
@@ -10,24 +13,34 @@ export const setupServer = () => {
   app.use(cors());
 
   app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-);
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
- app.get('/', (req, res) => {
-    res.send('Hello. Wellcome to contacts!');
+
+  app.get('/', (req, res) => {
+    const accept = req.headers.accept || '';
+    if (accept.includes('text/html')) {
+      res.send(welcome());
+    }
+    res.status(200).json({ message: 'Hello. Wellcome to contacts!' });
   });
-
 
   app.get('/contacts', async (req, res, next) => {
     try {
       const contacts = await getAllContacts();
-      res
-        .status(200)
-        .json({ message: 'Successfully found contacts!', data: contacts });
+
+      const accept = req.headers.accept || '';
+      if (accept.includes('text/html')) {
+        res.send(renderContactsList(contacts));
+      } else {
+        res
+          .status(200)
+          .json({ message: 'Successfully found contacts!', data: contacts });
+      }
     } catch (error) {
       next(error);
     }
@@ -37,13 +50,18 @@ export const setupServer = () => {
     try {
       const { contactId } = req.params;
       const contact = await getContactsById(contactId);
+      const accept = req.headers.accept || '';
       if (!contact) {
         return res.status(404).json({ message: 'Contact not found' });
       }
-      res.status(200).json({
-        message: `Successfully found contact with id ${contactId}!`,
-        data: contact,
-      });
+      if (accept.includes('text/html')) {
+        res.send(renderContact(contact));
+      } else {
+        res.status(200).json({
+          message: `Successfully found contact with id ${contactId}!`,
+          data: contact,
+        });
+      }
     } catch (error) {
       next(error);
     }
